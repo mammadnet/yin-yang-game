4c6bfb3a1337eb408d5853d9cb85ab848a9a4008
mammadnet <mamaddnet@gmail.com>
2023-08-10 18:20:24 +0330

    fix bug: black color continuity rule check

--- autoSolve.js
+++ autoSolve.js
@@ -23,38 +23,42 @@ function squareRule(tiles){
 // Continuity check for each color
 
 function continuity(tiles, number, color){
-    const queue = [];                           // To push every adjacent of current tile
-    const checked = {};
-    let counter = 0;                            // The number of tiles that are connected
-
-    const firstPos = findFirst(tiles, color);                   // Find the first place to start the algorithm
+    if(number > 0){
 
 
-    const flatPos = firstPos[0] * tiles.length + firstPos[1];
-    checked[flatPos] = true;
-    queue.push(firstPos);
-    counter++;
-
-    const dr = [0, 1, 0, -1];
-    const dc = [1, 0, -1, 0];
-
-    while(queue.length !== 0){
-
-        const [row, col] = queue.shift();
-
-        for(let d=0; d<4; d++){
-            let adjRow = row + dr[d];
-            let adjCol = col + dc[d];
-            if(tiles[adjRow]?.[adjCol] === color && checked[adjRow * tiles.length + adjCol] !== true){
-                    queue.push([adjRow, adjCol]);
-                    checked[adjRow * tiles.length + adjCol] = true;
-                    counter++
+        const queue = [];                           // To push every adjacent of current tile
+        const checked = {};
+        let counter = 0;                            // The number of tiles that are connected
+    
+        const firstPos = findFirst(tiles, color);                   // Find the first place to start the algorithm
+    
+    
+        const flatPos = firstPos[0] * tiles.length + firstPos[1];
+        checked[flatPos] = true;
+        queue.push(firstPos);
+        counter++;
+    
+        const dr = [0, 1, 0, -1];
+        const dc = [1, 0, -1, 0];
+    
+        while(queue.length !== 0){
+    
+            const [row, col] = queue.shift();
+    
+            for(let d=0; d<4; d++){
+                let adjRow = row + dr[d];
+                let adjCol = col + dc[d];
+                if(tiles[adjRow]?.[adjCol] === color && checked[adjRow * tiles.length + adjCol] !== true){
+                        queue.push([adjRow, adjCol]);
+                        checked[adjRow * tiles.length + adjCol] = true;
+                        counter++
+                }
             }
+    
         }
-
+        return counter === number;
     }
-
-    return counter === number;
+    return true;
 
 }
 
